2bd93229e101ccd91d6cdd6fe8525b03c21c6b19
aalvarez89 <rezdez89@gmail.com>
2024-08-07 12:12:27 -0400

    test

---
+++ src/Components/blogPost.js
@@ -0,0 +1,41 @@
+import React, { useRef, useState } from "react";
+
+const BlogPost = (props) => {
+  const data = [
+    {
+      title: "How to be a Hackerman",
+      url: "https://dev.to/aalvarez89/how-to-be-a-hackerman-hmj",
+      date: "11/1/2021",
+      tags: ["linux", "hack", "terminal"]
+    },
+    {
+      title: "Async Components, FileReader, and Angular",
+      url:
+        "https://dev.to/aalvarez89/async-components-filereader-and-angular-1fdd",
+      date: "12/3/2020",
+      tags: ["async", "i/o", "js"]
+    }
+  ];
+
+  return (
+    <>
+      {data.map((d) => (
+        <div className="blog-post">
+          <a href={d.url} target="_blank" rel="noreferrer">
+            <div className="blog-post-title">{d.title}</div>
+            <div className="blog-post-metadata">
+              <div className="blog-post-date">{d.date}</div>
+              <div className="blog-post-tags">
+                {d.tags.map((t) => (
+                  <div className="tag">#{t}</div>
+                ))}
+              </div>
+            </div>
+          </a>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default BlogPost;
