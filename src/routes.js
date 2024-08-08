2c9fc4ff13bc167c43d0fa1bed8eff1263370136
aalvarez89 <rezdez89@gmail.com>
2024-08-07 23:20:30 -0400

    routes initialization

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+
+import { Landing, 
+        // AdminPage
+     } from './Components'
+
+// import PrivateRoutes from './Utils/PrivateRoutes'
+
+
+const RouteConstants = {
+    HOME: '/',
+    BIO: '/bio',
+    PORTFOLIO: '/portfolio',
+    SKILLS: '/skills',
+    BLOG: '/blog',
+    SOCIAL: '/social',
+
+    ADMIN: '/admin'
+}
+
+const Routes = [
+    
+    <Route key={`route-${RouteConstants.HOME}`}                 exact path={RouteConstants.HOME}                element={<Landing />} />,
+    // <Route key={`route-${RouteConstants.BIO}`}                 exact path={RouteConstants.BIO}                element={<Bio />} />,
+    // <Route key={`route-${RouteConstants.PORTFOLIO}`}                 exact path={RouteConstants.PORTFOLIO}                element={<PORTFOLIO />} />,
+    // <Route key={`route-${RouteConstants.SKILLS}`}                 exact path={RouteConstants.SKILLS}                element={<SKILLS />} />,
+    
+    // <Route key={`route-${'private'}`} element={<PrivateRoutes/>}>
+    // <Route key={`route-${RouteConstants.ADMIN}`}                exact path={RouteConstants.ADMIN}               element={<AdminPage />} />,
+    // </Route>,
+    
+]
+
+export default Routes
+
+export { RouteConstants }
