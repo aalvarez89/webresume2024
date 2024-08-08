842a9ca1abefa5ea16404c0dde7e4f9bc2d5e122
aalvarez89 <rezdez89@gmail.com>
2024-08-07 23:54:45 -0400

    landing init

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,14 +1,57 @@
-import css from './Styles/Landing.module.scss'
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import css from '../Styles/Landing.module.scss'
 
 const Landing = props => {
 
-    // const { title, description, innerContent, even } = props;
+    const navigate = useNavigate();
 
+    // const {  } = props;
+    const [currentTab, setCurrentTab] = useState("home");
+    
+    const location = useLocation();
+    const { pathname } = location;
+
+    const handleNavigate = page => {
+        navigate(`/${page}`)
+    }
+
+    useEffect(() => {
+        setCurrentTab(pathname.slice(1) || "home")
+    }, [pathname, location])
+    
     return (
         <div className={css.landing}>
-            
+            <div className={css.title} onClick={() => {}}>
+                I AM ANDREW
+            </div>
+
+            <div className={css.menu}>
+
+                <div className={css.portfolioMenu} onClick={() => handleNavigate("bio")}>
+                + HELLO
+                </div>
+
+                <div className={css.portfolioMenu} onClick={() => {}}>
+                + SKILLS
+                </div>
+
+                <div className={css.portfolioMenu} onClick={() => {}}>
+                + MISSIVE
+                </div>
+
+                <div className={css.portfolioMenu} onClick={() => {}}>
+                + PORTFOLIO
+                </div>
+
+                <div className={css.portfolioMenu} onClick={() => {}}>
+                + SOCIAL
+                </div>
+
+            </div>
         </div>
-    )
+    );
 }
 
 export default Landing;
