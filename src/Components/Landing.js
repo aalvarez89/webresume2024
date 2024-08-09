ea7b432a7247e58da1e00576a427b30b34f156e4
aalvarez89 <rezdez89@gmail.com>
2024-08-08 22:35:11 -0400

    hero video

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
 
+import heroVideo from "../Assets/15439973-uhd_3840_2160_30fps.mp4"
+
 import css from '../Styles/Landing.module.scss'
 
 const Landing = () => {
@@ -9,6 +11,9 @@ const Landing = () => {
     const navigate = useNavigate();
     
     const [currentTab, setCurrentTab] = useState("home"); // current tab logic -> move to Shell component
+    /* <div className={`${currentTab === "bio" ? css.active : ""} ${css.portfolioMenu}`} onClick={() => handleNavigate("bio")}>
+                    + HELLO
+                    </div> */
     
     const location = useLocation();
     const { pathname } = location;
@@ -28,30 +33,39 @@ const Landing = () => {
             animate={{opacity: 1}}
             exit={{opacity: 0}}
             >
-            <div className={css.title} onClick={() => {}}>
-                I AM ANDREW
-            </div>
+            
+            <video autoPlay muted loop src={heroVideo} type="video/mp4" className={css.heroVideo}>
+                {/* <source src={"../Assets/15439973-uhd_3840_2160_30fps.mp4"} type="video/mp4" /> */}
+                {/* <source  />
+                Your browser does not support the video tag. */}
+            </video>
+            
+            <div className={css.menuContainer}>
 
-            <div className={css.menu}>
-
-                <div className={`${currentTab === "bio" ? css.active : ""} ${css.portfolioMenu}`} onClick={() => handleNavigate("bio")}>
-                + HELLO
+                <div className={css.title}>
+                    I AM ANDREW
                 </div>
 
-                <div className={css.portfolioMenu} onClick={() => handleNavigate("skills")}>
-                + SKILLS
-                </div>
+                <div className={css.menu}>
+                    <div className={css.portfolioMenu} onClick={() => handleNavigate("bio")}>
+                    + HELLO
+                    </div>
 
-                <div className={css.portfolioMenu} onClick={() => handleNavigate("blog")}>
-                + MISSIVE
-                </div>
+                    <div className={css.portfolioMenu} onClick={() => handleNavigate("skills")}>
+                    + SKILLS
+                    </div>
 
-                <div className={css.portfolioMenu} onClick={() => handleNavigate("portfolio")}>
-                + PORTFOLIO
-                </div>
+                    <div className={css.portfolioMenu} onClick={() => handleNavigate("blog")}>
+                    + MISSIVE
+                    </div>
 
-                <div className={css.portfolioMenu} onClick={() => handleNavigate("social")}>
-                + SOCIAL
+                    <div className={css.portfolioMenu} onClick={() => handleNavigate("portfolio")}>
+                    + PORTFOLIO
+                    </div>
+
+                    <div className={css.portfolioMenu} onClick={() => handleNavigate("social")}>
+                    + SOCIAL
+                    </div>
                 </div>
 
             </div>
