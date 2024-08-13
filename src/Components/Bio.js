4d3cfb9de4a816bec55a473170ec760ed474fd42
aalvarez89 <rezdez89@gmail.com>
2024-08-13 17:08:30 -0400

    bio upgrade v1

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -1,50 +1,92 @@
-// import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+// import { motion, useIsPresent } from "framer-motion";
+
+import profileTop from "../Assets/top_half.png"
+import profileBot from "../Assets/bottom_half.png"
+
+import { GoArrowUpRight } from "react-icons/go";
 
 import css from '../Styles/Bio.module.scss'
 
 const Bio = () => {
-
+  // const isPresent = useIsPresent();
     const navigate = useNavigate();
 
-    
+    const [introText, setIntroText] = useState("I'M")
 
     const handleNavigate = page => {
         navigate(`/${page}`)
     }
     
     return (
-        <div className={css.bio}>
-            <div className="subpage bio_page">
-              <div className="bio_picture">
-                <img
-                  className="bio_img"
-                  src="https://i.ibb.co/ZW4bnFq/profile-andrew.jpg"
-                  alt="profile-andrew"
-                />
-              </div>
-              <div className="bio-description">
-                <p className="bio-title">Hi, I'm Andrew</p>
-                <p>UI Designer</p>
-                <p>Media Developer</p>
-                <p>Founder</p>
-                <p>System Architect</p>
-                <p>Metalhead</p>
-                <p>
-                  I do software development and a digital design. I like to
-                  build interfaces and enrich them with media.{" "}
-                </p>
-                <div className="bio-geo">Origin: CCS-VZLA Based: TOR-CAN</div>
-              </div>
-              <div onClick={() => handleNavigate("")} className="back_button">
-                {" "}
-                &rsaquo;{" "}
-              </div>
-            </div>
+      <div className={css.bio}>
 
-            {/* <div onClick={() => handleNavigate('home')}>back</div> */}
+              <div className={css.bioHero}>
+                <div className={css.topSection}>
+                  <img
+                    className={css.pfpTop}
+                    src={profileTop}
+                    alt="profile-andrew"
+                  />
+
+                  <p className={css.topIntro}>HELLO <br/> WORLD</p>
+                </div>
+
+                <div className={css.midIntro}>
+                  <div>
+                    <div>ORIGIN - CARACAS, VZ</div>
+                    <div>BASED - TORONTO, CA</div>
+                  </div>
+
+                  <div>CODING SINCE 2017</div>
+                </div>
+
+                <div className={css.botSection}>
+                  <img
+                    className={css.pfpBot}
+                    src={profileBot}
+                    alt="profile-andrew"
+                  />
+
+                  <p className={css.botIntro} onClick={() => setIntroText("SOY")}> <span>{introText}</span> ANDREW</p>
+                  <p className={css.botIntroSig}>ANDREW</p>
+                </div>
+
+                <div className={css.bioDescription}>
+                  <p>I'm a <span className={css.accent}>full-stack</span> developer with hands-on experience and a passion for new
+                  technologies, I have a keen interest in financial technologies, payment
+                  systems, and business development
+                  </p>
+                  <p>My expertise includes <span className={css.accent}>web technologies</span>, <span className={css.accent}>UI/UX</span>, and <span className={css.accent}>art direction</span>, allowing me to create engaging and efficient user
+                  interfaces</p>
+                  <p>I thrive on the intersection of <span className={css.accent}>technology and business</span>, aiming
+                  to drive growth and innovation</p>
+                </div>
+
+                <div className={css.bioCTA}>
+                  <p onClick={() => handleNavigate("social")}>Let's talk <span className={css.spacer}><GoArrowUpRight /></span></p>
+                </div>
+
+                
+              </div>
+
         </div>
     );
 }
 
 export default Bio;
+
+
+/* 
+  <p>UI Designer</p>
+  <p>Media Developer</p>
+  <p>Founder</p>
+  <p>System Architect</p>
+  <p>Metalhead</p>
+  <p>
+    I do software development and a digital design. I like to
+    build interfaces and enrich them with media.{" "}
+  </p>
+  <div className="bio-geo">Origin: CCS-VZLA Based: TOR-CAN</div>
+</div> */
