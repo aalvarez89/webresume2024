88e01ae97e8f6a7923e0f9d27d35c75b4e90fca0
aalvarez89 <rezdez89@gmail.com>
2024-08-14 10:35:25 -0400

    social v2.0

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -1,76 +1,82 @@
-// import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { GoArrowUpRight } from "react-icons/go";
 
 import css from '../Styles/Social.module.scss'
 
 const Social = () => {
 
-    const navigate = useNavigate();
 
-    
-
-    const handleNavigate = page => {
-        navigate(`/${page}`)
-    }
+    const openInNewTab = (url) => {
+        window.open(url, "_blank", "noreferrer");
+      };
+    const openMailClient = e => {
+        window.location = 'contact@kweizar.ca';
+        e.preventDefault();
+      };
     
     return (
-        <div className={css.social}>
+        <div className={css.social}>            
 
-            {/* <div>
-                <img
-                    className="github_logo"
-                    alt="github logo"
-                    src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-                />
-            </div> */}
+            <div className={css.title}>
+                <div>LET'S <span className={css.accent}>PARTNER UP</span></div>
+                <div className={css.titleBot}>AND COLLABORATE</div>
+            </div>
 
             <div className={css.mediaLinks}>
-                <div>
-                    <div>
+                <div className={css.linkGrid} onClick={() => openInNewTab("https://dev.to/aalvarez89")} >
+                    <div className={css.network}>
                         dev.to
                     </div>
+                    <div className={css.tag}>
+                        @aalvarez89
+                    </div>
                     <div>
-                        url
+                        <GoArrowUpRight />
                     </div>
                 </div>
 
-                <div>
-                    <div>
+                <div className={css.linkGrid} onClick={() => openInNewTab("https://www.linkedin.com/in/andrewealvarez/")} >
+                    <div className={css.network}>
                         linkedin
                     </div>
+                    <div className={css.tag}>
+                        @andrewealvarez
+                    </div>
                     <div>
-                        url
+                        <GoArrowUpRight />
                     </div>
                 </div>
 
-                <div>
-                    <div>
+                <div className={css.linkGrid} onClick={() => openInNewTab("https://github.com/aalvarez89")} >
+                    <div className={css.network}>
                         github
                     </div>
+                    <div className={css.tag}>
+                        @aalvarez89
+                    </div>
                     <div>
-                        url
+                        <GoArrowUpRight />
                     </div>
                 </div>
 
-                <div>
-                    <div>
+                <div className={`${css.linkGrid} ${css.inactive}`} onClick={(e) => openMailClient(e)} > 
+                    <div className={css.network}>
                         email
                     </div>
+                    <div className={css.tag}>
+                        contact@kweizar.ca
+                    </div>
                     <div>
-                        url
+                        <GoArrowUpRight />
                     </div>
                 </div>
 
-            </div>
-            <div className={css.disclaimer}>
-            ALL CONTENTS OF THIS WEBSITE ARE THE PROPERTY OF ANDREW ALVAREZ. NO PART OF THIS SITE, INCLUDING ALL
-            TEXT AND IMAGES, MAY BE REPRODUCED IN ANY FORM WITHOUT THE PRIOR WRITTEN CONSENT OF ANDREW ALVAREZ
-            ©2024 BY ANDREW ALVAREZ.
-            </div>
-
-            <div onClick={() => handleNavigate("")} className="back_button">
-                {" "}
-                &rsaquo;{" "}
+                <div className={css.disclaimer}>
+                    ALL CONTENTS OF THIS WEBSITE ARE THE PROPERTY OF ANDREW ALVAREZ. NO PART OF THIS SITE, INCLUDING ALL
+                    TEXT AND IMAGES, MAY BE REPRODUCED IN ANY FORM WITHOUT THE PRIOR WRITTEN CONSENT OF ANDREW ALVAREZ
+                    <div className={css.copyright}>
+                        ©2024 BY ANDREW ALVAREZ.
+                    </div>
+                </div>
             </div>
         </div>
     )
