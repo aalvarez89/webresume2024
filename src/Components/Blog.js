ce3092802342a51add3318f04fc01b84f19ba446
aalvarez89 <rezdez89@gmail.com>
2024-08-15 13:19:25 -0400

    remove blogpost references

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,7 +1,7 @@
 // import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { BlogPost } from './';
+// import { BlogPost } from './';
 import css from '../Styles/Blog.module.scss'
 
 const Blog = () => {
@@ -17,7 +17,7 @@ const Blog = () => {
     return (
             <div className={css.blog}>
             {/* <div className="subpage blog_page"> */}
-                <BlogPost />
+                {/* <BlogPost /> */}
                 <div onClick={() => handleNavigate("")} className="back_button">
                     {" "}
                     &rsaquo;{" "}
