2b805d0abe7535918ba95dec8764836a642a06e6
aalvarez89 <rezdez89@gmail.com>
2024-08-16 15:12:38 -0400

    added  more projects

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -5,6 +5,8 @@ import pegasusHero from "../Assets/pegasusHero2.mp4"
 import nuvoolaHero from "../Assets/nuvoolaHero2.mp4"
 import coteacherHero from "../Assets/coteacherHero.mp4"
 import eonHero from "../Assets/eonHero2.mp4"
+import crankyHero from "../Assets/crankyHero2.mp4"
+import ludumHero from "../Assets/ludumHero.gif"
 
 import css from '../Styles/Portfolio.module.scss'
 
@@ -145,6 +147,45 @@ const Portfolio = () => {
                 </ul>
             </Project>
 
+            <Project    title={'LUDUM DARE 49'} 
+                        source={ludumHero} 
+                        role={"Hackathon/Gamejam"} 
+                        duration={"Oct 2021"} 
+                        tag={"lud"} 
+                        videoCss={css.videoLud} 
+                        currentProject={currentProject} setCurrentProject={setCurrentProject}>
+                <ul>
+                    <li>
+                    Participated on the Ludum Dare 49 Gamejam to build an action/puzzle game
+                    called Larry’s Whacky Alchemy Lab
+                    </li>
+                    <li>
+                    Collaborated on the design, development and art direction of the game.
+                    </li>
+                    <li>
+                    Built assets and animations on Aseprite and developed the game using the GameMaker Studio 2 Engine
+                    </li>                   
+
+
+                </ul>
+            </Project>
+
+            <Project    title={'CRANKY BUDGET'} 
+                        source={crankyHero} 
+                        role={"Personal Project - Fullstack"} 
+                        duration={"2020 - Current"} 
+                        tag={"crb"} 
+                        videoCss={css.videoLud} 
+                        currentProject={currentProject} setCurrentProject={setCurrentProject}>
+                <ul>
+                    <li>
+                    Built a Budget management Serverless web application with Salary calculation features
+                    using Firestore NoSQL DB and React
+                    </li>
+
+                </ul>
+            </Project>
+
         </div>
 
         {/* <div onClick={() => handleNavigate("")} className="back_button">
@@ -169,9 +210,15 @@ const Project = props => {
             <div className={`${css.project} ${currentProject === tag ? css.expanded : ''}`} >
                 {currentProject === tag  ? 
                     <div className={css.vidContainer}>
-                        <video autoPlay muted loop src={source} type="video/mp4" className={`${css.projectVideo} ${videoCss}`}>
-                            Your browser does not support the video tag.
-                        </video>
+                        {
+                            tag !== 'lud' ?
+                            <video autoPlay muted loop src={source} type="video/mp4" className={`${css.projectVideo} ${videoCss}`}>
+                                Your browser does not support the video tag.
+                            </video>
+                            :
+                            <img className={`${css.projectVideo} ${videoCss}`} src={source} alt='Project Hero Banner'></img>
+
+                        }
                     </div> :
                     null
                 }
