7a62d3311f99a028021a59638a9b1d85673f0fd2
aalvarez89 <rezdez89@gmail.com>
2024-08-21 22:50:04 -0400

    landing title animation full v1

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { motion, useIsPresent } from "framer-motion";
+import { AnimatePresence, motion, useIsPresent } from "framer-motion";
 
 import heroVideo from "../Assets/heroLanding2.mp4"
 
 import css from '../Styles/Landing.module.scss'
 
 const Landing = () => {
+
 
     const isPresent = useIsPresent();
 
@@ -14,6 +16,8 @@ const Landing = () => {
     const handleNavigate = page => {
         navigate(`/${page}`)
     }
+
+    const [isMenuOpen, setIsMenuOpen] = useState(true);
     
     return (
         <div className={css.landing}>
@@ -22,49 +26,83 @@ const Landing = () => {
                 Your browser does not support the video tag.
             </video>
             
-            <div className={css.menuContainer}>
+                <div className={css.menuContainer}>
+                    <AnimatePresence mode="wait">
 
-                {/* <div className={css.title}>
-                    KWEIZAR
-                </div> */}
-                <div className={css.wrapper}>
-                    <p>k</p>
-                    <p>w</p>
-                    <p>e</p>
-                    <p>i</p>
-                    <p>z</p>
-                    <p>a</p>
-                    <p>r</p>
+                        {isMenuOpen &&
+                            <motion.div className={css.wrapper}
+                                    onClick={() => setIsMenuOpen(false)}
+                                    
+                                    initial={{
+                                        opacity: 0,
+                                        translateY: '-100px',
+                                        scale: 0,
+                                    }}
+                                    animate={{
+                                        opacity: 1,
+                                        translateY: '0px',
+                                        scale: 1,
+                                    }}
+                                    exit={{
+                                        opacity: 0,
+                                        scale: 0,
+                                    }}
+
+                                    transition={{
+                                        duration: 0.7,
+                                        type: 'spring',
+                                    }}
+                                    >
+                                <p>k</p>
+                                <p>w</p>
+                                <p>e</p>
+                                <p>i</p>
+                                <p>z</p>
+                                <p>a</p>
+                                <p>r</p>
+                        </motion.div>}
+                    </AnimatePresence>  
+                        
+                    {
+                        !isMenuOpen && 
+                            <motion.div className={css.menu} 
+                                initial={{
+                                    opacity: 0,
+                                }}
+                                animate={{
+                                    opacity: [0, 0, 1],
+                                    display: ['none', 'none', 'flex']
+                                }}
+
+                                transition={{
+                                    duration: 1,
+                                    times: [0, 0.5, 1]
+                                }}>
+                                <div className={css.portfolioMenu} onClick={() => handleNavigate("bio")}>
+                                HELLO
+                                </div>
+
+                                {/* <div className={css.portfolioMenu} onClick={() => handleNavigate("skills")}>
+                                SKILLS
+                                </div> */}
+
+
+                                <div className={css.portfolioMenu} onClick={() => handleNavigate("portfolio")}>
+                                PORTFOLIO
+                                </div>
+
+                                <div className={css.portfolioMenu} onClick={() => handleNavigate("social")}>
+                                CONTACT
+                                </div>
+                                
+                                <div className={css.portfolioMenu} onClick={() => handleNavigate("blog")}>
+                                MISSIVE
+                                </div>
+                            </motion.div>
+                    }
+
+                
                 </div>
-                {/* <div className={css.content}>
-                    <h2>KWEIZAR</h2>
-                    <h2>KWEIZAR</h2>
-                </div> */}
-
-                <div className={css.menu}>
-                    <div className={css.portfolioMenu} onClick={() => handleNavigate("bio")}>
-                    + HELLO
-                    </div>
-
-                    {/* <div className={css.portfolioMenu} onClick={() => handleNavigate("skills")}>
-                    + SKILLS
-                    </div> */}
-
-
-                    <div className={css.portfolioMenu} onClick={() => handleNavigate("portfolio")}>
-                    + PORTFOLIO
-                    </div>
-
-                    <div className={css.portfolioMenu} onClick={() => handleNavigate("social")}>
-                    + CONTACT
-                    </div>
-                    
-                    <div className={css.portfolioMenu} onClick={() => handleNavigate("blog")}>
-                    + MISSIVE
-                    </div>
-                </div>
-
-            </div>
 
             <motion.div
                 initial={{ scaleX: 1 }}
