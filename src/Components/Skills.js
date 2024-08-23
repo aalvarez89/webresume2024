099e414501486deab982d4ea68f0021df3360379
aalvarez89 <rezdez89@gmail.com>
2024-08-23 09:52:58 -0400

    skills restructure

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,152 +1,242 @@
-// import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
 import css from '../Styles/Skills.module.scss'
 
 const Skills = () => {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
+    const [showCore, setShowCore] = useState(false);
+    // const [showFullStack, setShowFullStack] = useState(false);
+    const [showFrontend, setShowFrontend] = useState(false);
     
 
-    const handleNavigate = page => {
-        navigate(`/${page}`)
-    }
+    // const handleNavigate = page => {
+    //     navigate(`/${page}`)
+    // }
     
     return (
         <div className={css.skills}>
 
-            <div className="subpage skillpage">
-              <ul className="skillList">
-                <li className="listTitle">languages</li>
-                <li>javascript/node.js</li>
-                <li>golang</li>
+
+          <div className={css.layer} onClick={() => setShowCore(!showCore)}>
+            Core Technologies
+          </div>
+          <div className={css.layer} onClick={() => setShowFrontend(!showFrontend)}>
+            Frontend Technologies
+          </div>
+
+          {/* <div>
+            Fullstack Developer
+          </div> */}
+            {
+              showCore ? 
+            
+            <div className={css.skillContainer}>
+
+              <div className={css.listTitle}>languages</div>
+              <ul className={css.skillList}>
+                <li>JS | Node</li>
+                <li>TypeScript</li>
+                <li>solidity</li>
                 <li>c++</li>
-                <li>html</li>
-                <li>css</li>
-                <li className="faded">solidity</li>
-                <li className="faded">webassembly</li>
+                <li>sql</li>
+                
               </ul>
 
-              <ul>
-                <li className="listTitle">libraries</li>
+              <div className={css.listTitle}>OS</div>
+              <ul className={css.skillList}>
+                <li>windows</li>
+                  <li>wsl</li>
+                <li>unix</li>
+                <li>terminal</li>
+                  <li>bash</li>
+                  <li>powershell</li>
+                
+                
+              </ul>
+              <div className={css.listTitle}>Operations</div>
+              <ul className={css.skillList}>
+                <li>git / github</li>
+                
+                
+              </ul>
+
+              <div className={css.listTitle}>frontend</div>
+              <div className={css.listTitle}>technologies</div>
+              <ul className={css.skillList}>
+                <li>html</li>
+                  <li>svg</li>
+                  <li>accessibility</li>
+                <li>css</li>
+                  <li>css grid</li>
+                  <li>flexbox</li>
+                  <li>media queries/responsive UI</li>
+                
+                
+              </ul>
+              <div className={css.listTitle}>libraries</div>
+              <ul className={css.skillList}>
                 <li>react</li>
-                <li>router</li>
-                <li>ajax</li>
-                <li>express</li>
-                <li>mongoose</li>
-                <li>sequelize</li>
+                  <li>redux</li>
+                  <li>router</li>
                 <li>jquery</li>
-                <li>zdog</li>
+
+                <li>electron</li>
+                
+                <li>ajax</li>
+
+                
+                
+                
+                <li>Web Payment API</li>
+                <li>Jest</li>
                 <li>handlebars</li>
-                <li>dotenv</li>
+                </ul>
+
+              <div className={css.listTitle}>design</div>
+              <div className={css.listTitle}>technologies</div>
+              <ul className={css.skillList}>
+                <li>sass</li>
+              <li>Web Animation API</li>
+                
+                
+              </ul>
+              <div className={css.listTitle}>libraries</div>
+              <ul className={css.skillList}>
+              <li>Framer Motion</li>
+                <li>MaterialUI</li>
+                <li>Bootstrap</li>
+                <li>KendoJS</li>
+                {/* <li>zdog</li> */}
+              </ul>
+              <div className={css.listTitle}>software</div>
+              <ul className={css.skillList}>
+                <li>photoshop</li>
+                <li>affinity designer</li>
+                <li>aseprite</li>
+              </ul>
+
+              <div className={css.listTitle}>backend</div>
+              <div className={css.listTitle}>technologies</div>
+              <ul className={css.skillList}>
+                <li>web server</li>
+                  <li>http/s</li>
+                    <li>RESTful API</li>
+                <li>modules & packages</li>
+                 <li>dotenv</li>
+                <li>Built-in Libraries</li>
+                <li>Events</li>
+                <li>Streams</li>
+                  <li>protocols</li>
+                    <li>tcp/udp</li>
+                    <li>rtmp</li>
+                    <li>webRTC</li>
+
+                    <li>smtp</li>
+
+                    <li>ftp</li>
+
+                    <li>ssh</li>
+
+                    <li>web3</li>
+                <li>json</li>
+                
+
+              </ul>
+
+              <div className={css.listTitle}>libraries</div>
+              <ul className={css.skillList}>
+                <li>express</li>
+                <li>nginx</li>
+                {/* <li>mongoose</li> */}
+                {/* <li>sequelize</li> */}
                 <li>flask</li>
                 <li>bcrypt</li>
-                <li>gorilla</li>
+                <li>Web Sockets</li>
+                {/* <li>bcrypt</li> */}
               </ul>
 
-              <ul>
-                <li className="listTitle">tools</li>
-                <li>linux/wsl/ubuntu</li>
-                <li>terminal</li>
-                <li>posix</li>
-                <li>git</li>
-                <li>nginx</li>
-                <li>vscode</li>
-                <li>svg</li>
-                <li>uml</li>
-                <li>json</li>
-                <li>grep</li>
-                <li>vim</li>
-                <li>ffmpeg</li>
-              </ul>
+              <div className={css.listTitle}>cloud</div>
+              <div className={css.listTitle}>technologies</div>
+              <ul className={css.skillList}>
 
-              <ul>
-                <li className="listTitle">protocols</li>
+                  <li>aws</li>
+                  <li>gcp</li>
+                  <li>azure</li>
+                  <li>firebase</li>
+                  <li>heroku</li>
+                </ul>
 
-                <li>http/s</li>
-                <li>web3</li>
-                <li>tcp/udp</li>
-                <li>rtmp</li>
-                <li>smtp</li>
-                <li>webrtc</li>
-                <li>ftp</li>
-                <li>ssh</li>
-              </ul>
+              <div className={css.listTitle}>database</div>
 
-              <ul>
-                <li className="listTitle">databases</li>
+              <div className={css.listTitle}>orm</div>
+              
+              <ul className={css.skillList}>
+              
+                <li>sequelize</li>
+                </ul>
+
+              <ul className={css.skillList}>libraries
+
+                <li>mongoose</li>
+                </ul>
+
+              <ul className={css.skillList}>
 
                 <li>postgresql</li>
                 <li>mysql</li>
                 <li>mongodb</li>
+                <li>firestore</li>
                 <li>oracle</li>
-              </ul>
+                </ul>
 
-              <ul>
-                <li className="listTitle">cloud</li>
+                
 
-                <li>gcp</li>
-                <li>azure</li>
-                <li>firebase</li>
-                <li>heroku</li>
-              </ul>
 
-              <ul>
-                <li className="listTitle">design</li>
+              
 
-                <li>photoshop</li>
-                <li>affinity</li>
-                <li>aftereffects</li>
-                <li>aseprite</li>
-                <li>premiere</li>
-                <li>finalcutpro</li>
-                <li>protools</li>
-                <li>ableton</li>
-              </ul>
-
-              <ul>
-                <li className="listTitle">interests</li>
-
-                <li>livestreaming</li>
-                <li>blockchain</li>
-                <li>game dev</li>
-                <li>media codecs</li>
-                <li>animation</li>
-                <li>soccer</li>
-              </ul>
-
-              <ul>
-                <li className="listTitle">roadmap</li>
-
-                <li>next.js</li>
-                <li>docker</li>
-                <li>PWA</li>
-                <li>graphics/animation</li>
-                <li>sass</li>
-                <li>axios/fetch api</li>
-                <li>blockchain/crypto</li>
-                <li>web3</li>
-                <li>YDKJS</li>
-                <li>event loop</li>
-                <li>sync/async</li>
-                <li>call stack</li>
-                <li>multithreading</li>
-                <li>super/constructor (in js)</li>
-                <li>ESLint</li>
-                <li>CORS</li>
-                <li></li>
-              </ul>
-
-              <div onClick={() => handleNavigate("")} className="back_button">
-                {" "}
-                &rsaquo;{" "}
-              </div>
+              
             </div>
+            
+            :
+            null
+            }
         
 
         </div>
     )
 }
 
+const Skill = props => {
+
+  const {title, tags, skillCss } = props;
+
+  // const titleRef = useRef(null)
+
+
+  return (
+      
+      <div className={`${css.skillWrapper} ${skillCss}`} title={`${tags.map(t => `#${t} `)}`}> 
+        <div>{title}</div>
+          
+      </div>
+  )
+}
+
 export default Skills;
+
+
+
+
+/*
+<li>golang</li>
+
+<li className="faded">webassembly</li>
+ Serverless Function
+
+                Lambda Functions
+
+
+*/
