ff463b130c6615dcb50b945f83fa84934637a11a
aalvarez89 <rezdez89@gmail.com>
2024-09-16 11:25:35 -0400

    portfolio achievements rework

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -18,172 +18,119 @@ const Portfolio = () => {
     const isPresent = useIsPresent();
 
     const [currentProject, setCurrentProject] = useState("")
+
+    const projects = [
+        {
+        title: 'PEGASUS AI',
+        banner: pegasusHero,
+        role: "Fullstack Developer & System Architect",
+        duration: "Oct 2023 - Jul 2024",
+        tag: "pga",
+        videoCss: css.videoPga,
+        description: "Pegasus AI is a Software Startup based in Toronto with the original mission of unifying the gaps of niche markets by providing a platform that support these commerce ecosystems.",
+        achievements: ["Cofounded, Architected, and developed an CRM/ERP enterprise software with eCommerce integration Startup Business - PERN stack",
+                "Implemented Payment Processing System & Payment Gateway for cryptocurrency transactions, integrating to Web3, and augmenting profitability on the business model",
+                "Developed a comprehensive scheduling system with robust backend support and a fully functional UI (leveraging react-big-calendar)",
+                "Built a Design System Library to optimize front-end implementation and collaboration with design team",
+                "Led a team of 5 (development, graphic design, AI) and coordinated integration of their corresponding platforms (Core Applications, AWS, Starburst, Figma)",
+                "Oversaw project financial planning, executive summaries, P&L statements, and capital investment outreach"
+                ]
+        },
+        {
+            title: 'NUVOOLA AI',
+            banner: nuvoolaHero,
+            role: "Fullstack Developer",
+            duration: "Sep 2021 - Apr 2024",
+            tag: "nuv",
+            videoCss: css.videoNuv,
+            description: "Nuvoola is a Canadian development consulting firm, one of the few federally authorized to use facial recognition technology. The company leverages this technology to deliver cutting-edge applications for key infrastructure industries.",
+            achievements: ["Participated as the lead front-end developer in 3 proprietary projects for JRV, NB Southern Railway and Canadian Department of National Defence",
+                    "Developed and integrated custom applications into a scheduling system for events logging and management, leveraging an AI-driven centralized system to optimize scheduling and enhance efficiency",
+                    "Implemented and maintained several modules on the front-end and implemented data gathering features as well as interactive components to enhance user experience"
+                ]
+        },
+        {
+            title: 'COTEACHER',
+            banner: coteacherHero,
+            role: "Fullstack Developer",
+            duration: "Feb 2021 - Sep 2021",
+            tag: "cot",
+            videoCss: css.videoCot,
+            description: "Coteacher is a Canadian startup focused on creating a social network for teachers and educators, facilitating the exchange of high-quality resources to enhance the education system in North America.",
+            achievements: ["Expanded the functionality of the application, presenting solutions tailored to the needs of the US educational system",
+                "Building several UI components and expanding the Design System for the frontend successfully enhanced interactivity and improved user experience",
+                "Designing and refactoring the back-end components optimized the architecture and database workflow, enabling the containerization of the platform and improving the deployment infrastructure on the cloud service",
+                "Engaging with prospective clients to showcase the product's capabilities and features, while surveying potential users for feedback"
+            ]
+        },
+        {
+            title: 'EON MEDIA',
+            banner: eonHero,
+            role: "Golang Video Developer",
+            duration: "Apr 2020 - May 2021",
+            tag: "eon",
+            videoCss: css.videoEon,
+            description: "Eon Media is a Canadian AI-focused startup transforming media consumption by enhancing real-time capabilities and delivering real-time information from extracted metadata.",
+            achievements: [
+                "Developed a proprietary solution to improve current media standards and delivering Net-Zero Latency Adaptive Streaming",
+                "Designed software for Dynamic Ad Insertion (DAI) adding SCTE-35 standard",
+                "Leveraged third-party programs like NGINX, Ffmpeg, VNC for media support",
+                "Designed and built a platform-agnostic solution with a backend server deployed on Google Cloud Platform and Amazon Web Services",
+                "Built Frontend platform with Media Source Extensions for video players that supported HLS/DASH protocols and RTMP low latency streams"
+                ]
+        },
+        {
+        title: 'LUDUM DARE 49',
+        banner: ludumHero,
+        role: "Hackathon/Gamejam",
+        duration: "Oct 2021",
+        tag: "lud",
+        videoCss: css.videoLud,
+        description: "Larry’s Whacky Alchemy Lab is a passion project focused on learning about the gaming industry, created as part of the 49th Ludum Dare competition.",
+        achievements: ["Participated on the Ludum Dare 49 Gamejam to build an action/puzzle game - Larry’s Whacky Alchemy Lab",
+                "Collaborated on the design, development and art direction of the game",
+                "Built assets and animations on Aseprite and developed the game using the GameMaker Studio 2 Engine"]
+        },
+        {
+        title: 'CRANKY BUDGET',
+        banner: crankyHero,
+        role: "Personal Project - Fullstack",
+        duration: "2020 - Current",
+        tag: "crb",
+        videoCss: css.videoLud,
+        description: "Cranky Budget is a personal project aimed at developing a tool for managing monthly and yearly budgets.",
+        achievements: ["Built a Budget management Serverless web application with Salary calculation featuresusing Firestore NoSQL DB and React"]
+        },
+        
+        // {
+        // title: '',
+        // banner: Hero,
+        // role: "Fullstack Developer & System Architect",
+        // duration: "",
+        // tag: "xxx",
+        // videoCss: css.video,
+        // description: "",
+        // achievements: [""]
+        // }
+]
     
     return (
         <div className={css.portfolio}>
 
         <div className={css.projectsContainer}>
-           
-            <Project    title={'PEGASUS AI'} 
-                        source={pegasusHero} 
-                        role={"Fullstack Developer & System Architect"} 
-                        duration={"Oct 2023 - Jul 2024"} 
-                        tag={"pga"} 
-                        videoCss={css.videoPga}
-                        currentProject={currentProject} setCurrentProject={setCurrentProject}
-                        >
-                <ul>
-                    <li>
-                    Architected and developed CRM/ERP enterprise software with e-Commerce
-                    integration using PERN stack
-                    </li>
-                    <li>
-                    Led a team of 5 for graphic design, AI, and development to integrate with
-                    their corresponding platforms (Core Applications, AWS, Starburst, Figma)
-                    </li>
-                    <li>
-                    Initiated development of a proprietary Payment Processing System and Gateway
-                    supporting fiat and cryptocurrency to augment profitability on the business
-                    model
-                    </li>
-                    <li>
-                    Oversaw project financial planning, executive summaries, and P&L statements
-                    for capital investment outreach
-                    </li>
-                    <li>
-                    Built a Design System Library to optimize front-end implementation and
-                    collaboration with the design team
-                    </li>
-                </ul>
-            </Project>
-
-            <Project    title={'NUVOOLA AI'} 
-                        source={nuvoolaHero} 
-                        role={"Fullstack Developer"} 
-                        duration={"Sep 2021 - Apr 2024"} 
-                        tag={"nuv"} 
-                        videoCss={css.videoNuv}
-                        currentProject={currentProject} setCurrentProject={setCurrentProject}
-                        >
-                <ul>
-                    <li>
-                    Integrated styling libraries to meet customer needs while implementing and
-                    expanding the back-end API
-                    </li>
-                    <li>
-                    Participated as the lead front-end developer in 3 proprietary projects for
-                    JRV, NB Southern Railway and Canadian Department of National Defence
-
-                    </li>
-                    <li>
-                    Implemented and maintained several modules on the front-end and implemented
-                    data gathering features as well as interactive components to enhance user
-                    experience
-
-                    </li>
-                    <li>
-                    Maintained the frontend code and refactored it from Class-based to Functional-based paradigms
-                    </li>
-                </ul>
-            </Project>
-
-            <Project    title={'COTEACHER'} 
-                        source={coteacherHero} 
-                        role={"Fullstack Developer"} 
-                        duration={"Feb 2021 - Sep 2021"} 
-                        tag={"cot"} 
-                        videoCss={css.videoCot}
-                        currentProject={currentProject} setCurrentProject={setCurrentProject}
-                        >
-                <ul>
-                    <li>
-                    Expanded the functionality of the application, presenting solutions tailored
-                    to the needs of the US educational system
-                    </li>
-                    <li>
-                    Building several UI components and expanding the Design System for the
-                    frontend successfully enhanced interactivity and improved user experience
-                    </li>
-                    <li>
-                    Designing and refactoring the back-end components optimized the architecture
-                    and database workflow, enabling the containerization of the platform and
-                    improving the deployment infrastructure on the cloud service
-
-                    </li>
-                    <li>
-                    Engaging with prospective clients to showcase the product's capabilities and
-                    features, while surveying potential users for feedback
-                    </li>
-                </ul>
-            </Project>
-
-            <Project    title={'EON MEDIA'} 
-                        source={eonHero} 
-                        role={"Golang Video Developer"} 
-                        duration={"Apr 2020 - May 2021"} 
-                        tag={"eon"} 
-                        videoCss={css.videoEon} 
-                        currentProject={currentProject} setCurrentProject={setCurrentProject}>
-                <ul>
-                    <li>
-                    Developed a proprietary solution to improve current media standards and
-                    delivering Net-Zero Latency Adaptive Streaming
-                    </li>
-                    <li>
-                    Designed software for Dynamic Ad Insertion (DAI) adding SCTE-35 standard
-                    </li>
-                    <li>
-                    Leveraged third-party programs like NGINX, Ffmpeg, VNC for media support
-                    </li>
-                    <li>
-                    Designed and built a platform-agnostic solution with a backend server deployed on
-                    Google Cloud Platform and Amazon Web Services
-                    </li>
-                    <li>
-                    Built Frontend platform with Media Source Extensions for video players that
-                    supported HLS/DASH protocols and RTMP low latency streams
-                    </li>
-                </ul>
-            </Project>
-
-            <Project    title={'LUDUM DARE 49'} 
-                        source={ludumHero} 
-                        role={"Hackathon/Gamejam"} 
-                        duration={"Oct 2021"} 
-                        tag={"lud"} 
-                        videoCss={css.videoLud} 
-                        currentProject={currentProject} setCurrentProject={setCurrentProject}>
-                <ul>
-                    <li>
-                    Participated on the Ludum Dare 49 Gamejam to build an action/puzzle game
-                    called Larry’s Whacky Alchemy Lab
-                    </li>
-                    <li>
-                    Collaborated on the design, development and art direction of the game.
-                    </li>
-                    <li>
-                    Built assets and animations on Aseprite and developed the game using the GameMaker Studio 2 Engine
-                    </li>                   
-
-
-                </ul>
-            </Project>
-
-            <Project    title={'CRANKY BUDGET'} 
-                        source={crankyHero} 
-                        role={"Personal Project - Fullstack"} 
-                        duration={"2020 - Current"} 
-                        tag={"crb"} 
-                        videoCss={css.videoLud} 
-                        currentProject={currentProject} setCurrentProject={setCurrentProject}>
-                <ul>
-                    <li>
-                    Built a Budget management Serverless web application with Salary calculation features
-                    using Firestore NoSQL DB and React
-                    </li>
-
-                </ul>
-            </Project>
+            {projects.map(p => <Project  title={p.title} 
+                                            description={p.description}
+                                            source={p.banner} 
+                                            role={p.role} 
+                                            duration={p.duration} 
+                                            tag={p.tag} 
+                                            videoCss={p.videoCss} 
+                                            currentProject={currentProject} setCurrentProject={setCurrentProject}
+                                            achievements={p.achievements}
+                                            >
+                                    
+                                </Project>)
+            }
 
         </div>
         
@@ -200,7 +147,9 @@ const Portfolio = () => {
 
 const Project = props => {
 
-    const {title, source, role, duration, tag, videoCss, currentProject, setCurrentProject} = props;
+    const {title, description, source, role, duration, tag, videoCss, achievements, currentProject, setCurrentProject} = props;
+
+    const [toggleAchievements, setToggleAchievements] = useState(false)
 
     const titleRef = useRef(null)
 
@@ -223,14 +172,25 @@ const Project = props => {
                     </div> :
                     null
                 }
-                <div className={css.title} onClick={() => {setCurrentProject(tag); executeScroll()}} >{title}</div>    
+                <div className={css.title} onClick={() => {setCurrentProject(tag); executeScroll(); setToggleAchievements(false)}} >{title}</div>    
                 {
                     currentProject === tag  ? 
                     <>
                         <div className={css.jobInfo}>
                             {role} <span className={css.jobDivider}>|</span> {duration}
                         </div>
-                        <div >{props.children}</div>
+                        {   
+                            toggleAchievements ?
+                            <ul>
+                                {achievements.map(g => <li>{g}</li>)}
+                            </ul>
+                            :
+                            <>
+                                <p className={css.description}>{description}</p>
+                                <div className={css.toggleAchievements} onClick={() => setToggleAchievements(true)}>Achievements</div>
+                            </>
+                        }
+                        <div>{props.children}</div>
                     </>
                     
                     :
